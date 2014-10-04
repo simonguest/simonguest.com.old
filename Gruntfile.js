d3a5e3273f2a81a44533f9c4cb71d0c1c79e8f5a
Simon Guest <simon.guest@concur.com>
2014-10-04 08:13:57 -0700

    Added simple grunt commands to perform deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,32 @@
 module.exports = function (grunt) {
-    require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-    grunt.initConfig({
-        notify: {
-            preview: {
-                options: {
-                    message: 'Blog now running in preview mode!'
-                }
-            }
-        },
-        shell: {
-            preview: {
-                command: 'wintersmith preview'
-            }
+  grunt.initConfig({
+    notify: {
+      preview: {
+        options: {
+          message: 'Blog now running in preview mode!'
         }
-    });
+      }
+    },
+    shell: {
+      preview: {
+        command: 'wintersmith preview'
+      },
+      build: {
+        command: 'wintersmith build'
+      },
+      deploy: {
+        command: ['git add -A','git commit -m "Updated Pages"','git push'].join('&&'),
+        options: {
+          execOptions: {
+            cwd: 'build'
+          }
+        }
+      }
+    }
+  });
 
-    // Default task(s).
-    grunt.registerTask('default', ['npm-install','notify:preview', 'shell:preview']);
+  grunt.registerTask('default', ['npm-install', 'notify:preview', 'shell:preview']);
+  grunt.registerTask('deploy', ['shell:build', 'shell:deploy']);
 };
